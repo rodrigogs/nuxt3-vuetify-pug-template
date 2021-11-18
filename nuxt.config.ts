451c549ae85175c711cc84841fd4aa97a6b86274
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2021-11-18 12:22:00 -0300

    fixed css reference

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ['vuetify/lib/styles/main.sass'],
+  css: ['assets/css/main.css'],
   build: {
     transpile: ['vuetify'],
   },
